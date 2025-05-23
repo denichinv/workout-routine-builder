ce2e0f25bdf9773e40f8b5018f47b5d114199ee1
Vilizar Denichin <denichinv@gmail.com>
2025-05-23 22:08:26 +0100

    changed the layout of the page

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
           </p>
         </header>
 
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 gap-6">
           <div className="lg:col-span-1">
             <ExerciseList
               exercises={exercises}
